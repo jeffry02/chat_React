2809ce42cee2d3167c112efee968ed6474ead7d4
Allan Martinez <jeffrymart02@gmail.com>
2023-06-30 13:25:02 -0600

    change the if and fix the messages color

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,21 +5,17 @@ import {useUser} from '../context/auth.context'
 export default function Message({message}) {
   const user = useUser()
   const isAuthUser = user.id
-  var messa = {'self-end': 10} 
-  if (isAuthUser != message.sender_id) { 
-    messa = {'self-start': 10}
-  }
- 
+  const messa = isAuthUser !== message.sender_id ? {'self-start': 10} : {'self-end': 10};
 
   return (
     <div className={clsx('flex items-end space-x-2', messa)}>
       <div
         className={clsx('flex flex-col gap-y-2.5 rounded-xl p-3.5', {
-          'rounded-bl-none bg-gray-100': !isAuthUser,
-          'rounded-br-none bg-green-300': isAuthUser,
+          'rounded-bl-none bg-gray-200': messa['self-start'],
+          'rounded-br-none bg-green-300': messa['self-end'],
         })}
       >
-        {!isAuthUser && (
+        { messa && (
           <span className="text-xs text-orange-500">{message.sender.name}</span>
         )}
         {message.message}
